cc438411892268f27a9d9e41e94bb991e0512350
Izzat <spartan_genocide@hotmail.com>
2016-09-30 11:52:09 +0800

    Latest Db
    With recommendation

--- ict3104.sql
+++ ict3104.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 26, 2016 at 03:06 PM
+-- Generation Time: Sep 30, 2016 at 05:51 AM
 -- Server version: 5.6.20
 -- PHP Version: 5.5.15
 
@@ -64,7 +64,7 @@ CREATE TABLE IF NOT EXISTS `hod` (
 --
 
 INSERT INTO `hod` (`hodid`, `hodname`, `hodemail`, `password`, `remember_token`, `created_at`, `updated_at`) VALUES
-(1, 'hod', 'hod@hod.com', '$2y$10$ADd3MMvLklcRfOb1oC5JD.xF8.h3P6rfogkETuf8/z.1cnmqYu4wi', '0FvOUrhnP5Jez7hJ1jHR2SHYUrDbLOEtShVP1ex3REc7A5Nj820mP1d4ephO', NULL, '2016-09-26 04:17:02');
+(1, 'hod', 'hod@hod.com', '$2y$10$ADd3MMvLklcRfOb1oC5JD.xF8.h3P6rfogkETuf8/z.1cnmqYu4wi', 'o0Dozp30uKvoiI1oeeNX7Ou1GOGhqUlOe0E8gW0LXSUFvs2BbiDm4c7G6kXt', NULL, '2016-09-29 18:37:41');
 
 -- --------------------------------------------------------
 
@@ -87,7 +87,52 @@ CREATE TABLE IF NOT EXISTS `lecturer` (
 --
 
 INSERT INTO `lecturer` (`lecturerid`, `lecturername`, `lectureremail`, `password`, `remember_token`, `created_at`, `updated_at`) VALUES
-(1, 'lecturer', 'lecturer@lecturer.com', '$2y$10$ADd3MMvLklcRfOb1oC5JD.xF8.h3P6rfogkETuf8/z.1cnmqYu4wi', 'TUg2pq8GKDkQHnPEBJO75uqPmPr8mYMUkcYanyU17JGVMsG84kHwLqGtEB78', NULL, '2016-09-26 04:17:11');
+(1, 'lecturertest', 'lecturer@lecturer.com', '$2y$10$ADd3MMvLklcRfOb1oC5JD.xF8.h3P6rfogkETuf8/z.1cnmqYu4wi', 'mGx4iw49IatnHqHBjlyQMFo5uMaQjvdRM6dBKmAQ3vAjYGt4Z4OLxlf3X9BA', NULL, '2016-09-28 00:23:44');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `module`
+--
+
+CREATE TABLE IF NOT EXISTS `module` (
+`id` int(10) NOT NULL,
+  `modulename` varchar(255) NOT NULL,
+  `description` varchar(255) DEFAULT NULL,
+  `lecturerid` int(10) NOT NULL,
+  `hodid` int(10) NOT NULL
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
+
+--
+-- Dumping data for table `module`
+--
+
+INSERT INTO `module` (`id`, `modulename`, `description`, `lecturerid`, `hodid`) VALUES
+(1, 'Maths', 'E Maths', 1, 1),
+(2, 'Science', 'Physics', 1, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `recommendation`
+--
+
+CREATE TABLE IF NOT EXISTS `recommendation` (
+`id` int(10) NOT NULL,
+  `recommendation` varchar(255) DEFAULT NULL,
+  `studentid` int(10) NOT NULL,
+  `lecturerid` int(10) NOT NULL,
+  `hodid` int(11) NOT NULL,
+  `moduleid` int(11) NOT NULL
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
+
+--
+-- Dumping data for table `recommendation`
+--
+
+INSERT INTO `recommendation` (`id`, `recommendation`, `studentid`, `lecturerid`, `hodid`, `moduleid`) VALUES
+(1, 'Very Good', 1, 1, 1, 1),
+(2, 'Excellent', 1, 1, 1, 2);
 
 -- --------------------------------------------------------
 
@@ -110,7 +155,7 @@ CREATE TABLE IF NOT EXISTS `students` (
 --
 
 INSERT INTO `students` (`studentid`, `studentname`, `studentemail`, `password`, `remember_token`, `created_at`, `updated_at`) VALUES
-(1, 'student', 'student@student.com', '$2y$10$ADd3MMvLklcRfOb1oC5JD.xF8.h3P6rfogkETuf8/z.1cnmqYu4wi', 'clbhCZClN2Na4TrrSdHgTeteFWpd6nf3H0gww39j2MuRjmH8GFfTpqkp4l8S', NULL, '2016-09-26 04:49:56');
+(1, 'student2', 'student@student.com', '$2y$10$ADd3MMvLklcRfOb1oC5JD.xF8.h3P6rfogkETuf8/z.1cnmqYu4wi', 'j10XzusfCKQogmEz9sYOWSrV99wvNEPqmoYuoPYuxZ829DEHYZVgDVdQ1Fc2', NULL, '2016-09-29 01:45:12');
 
 -- --------------------------------------------------------
 
@@ -133,7 +178,7 @@ CREATE TABLE IF NOT EXISTS `users` (
 --
 
 INSERT INTO `users` (`id`, `name`, `email`, `password`, `remember_token`, `created_at`, `updated_at`) VALUES
-(1, 'test', 'admin@admin.com', '$2y$10$ADd3MMvLklcRfOb1oC5JD.xF8.h3P6rfogkETuf8/z.1cnmqYu4wi', 'b2ntiquiTnuKBWA8Mr3OpLxC47oVCUIXr0f2v1zFw9PWwjQPYP5CDIchalCM', NULL, '2016-09-26 04:59:26');
+(1, 'test', 'admin@admin.com', '$2y$10$ADd3MMvLklcRfOb1oC5JD.xF8.h3P6rfogkETuf8/z.1cnmqYu4wi', 'NRnJ2vpiEq5Vk2lQ9TTPJEY9mh7wP0mz02EfwwkuUjewrCYuVznZcAVtHV9W', NULL, '2016-09-28 00:26:07');
 
 --
 -- Indexes for dumped tables
@@ -156,6 +201,18 @@ ALTER TABLE `hod`
 --
 ALTER TABLE `lecturer`
  ADD PRIMARY KEY (`lecturerid`);
+
+--
+-- Indexes for table `module`
+--
+ALTER TABLE `module`
+ ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `recommendation`
+--
+ALTER TABLE `recommendation`
+ ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `students`
@@ -188,6 +245,16 @@ MODIFY `hodid` int(10) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
 --
 ALTER TABLE `lecturer`
 MODIFY `lecturerid` int(10) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
+--
+-- AUTO_INCREMENT for table `module`
+--
+ALTER TABLE `module`
+MODIFY `id` int(10) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
+--
+-- AUTO_INCREMENT for table `recommendation`
+--
+ALTER TABLE `recommendation`
+MODIFY `id` int(10) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
 --
 -- AUTO_INCREMENT for table `students`
 --
